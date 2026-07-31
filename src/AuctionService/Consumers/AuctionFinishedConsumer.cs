8cbe5f623384c94d92dedc216d1d08a8ed8a4c5d
MarekVavrovic <vavrovic@gmail.com>
2026-07-31 17:33:42 +0100

    11 Bid Service, gRPC

--- src/AuctionService/Consumers/AuctionFinishedConsumer.cs
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -11,7 +11,7 @@ public class AuctionFinishedConsumer(AuctionDbContext dbContext) : IConsumer<Auc
     public async Task Consume(ConsumeContext<AuctionFinished> context)
     {
         //auction that has finished
-         var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId) 
+         var auction = await dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId)) 
             ?? throw new InvalidOperationException($"Auction with ID {context.Message.AuctionId} not found.");
         
         if (context.Message.ItemSold)
--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,6 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
+using AuctionService.Services;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +16,7 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+//MassTransit
 builder.Services.AddMassTransit(x =>
 {
     x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
@@ -40,7 +42,7 @@ builder.Services.AddMassTransit(x =>
     });
 });
 
-//JWT
+//Authentication - JWT
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -50,7 +52,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 
+//unit tests
 builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
@@ -59,6 +63,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
 
 //seeding data for auction
 try
